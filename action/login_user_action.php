5f2b0e5cf77fe4b0c84376cac844ea36fb727e98
Victor  Quagraine <Victorquagraine9@gmail.com>
2024-02-23 04:39:23 +0000

    Renamed manage.html to add_chore_view.html

--- action/login_user_action.php
+++ action/login_user_action.php
@@ -12,16 +12,16 @@ if (isset($_POST['login-submit'])){
     $result = $con->query($query);
 
     if($result->num_rows < 1){
-        echo '<script>window.location.href="../login/login_view.php";</script>';
         echo '<script>alert("Incorrect email!");</script>';
+        echo '<script>window.location.href="../login/login_view.php";</script>';
         exit();
     } else{
         $row = $result->fetch_assoc();
         $hash_pwd = $row['passwd'];
         $hash = password_verify($pwd, $hash_pwd);
         if(!$hash){
-            echo '<script>window.location.href="../login/login_view.php";</script>';
             echo '<script>alert("Incorrect Password!");</script>';
+            echo '<script>window.location.href="../login/login_view.php";</script>';
             exit();
         }
         else{
